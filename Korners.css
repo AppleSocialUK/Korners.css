a05feb7d92a795a5a09bd2fe56e883a12f2926f5
AppleSocialUK <jkjameskenny@gmail.com>
2013-04-26 19:20:33 +0200

    Create Korners.css

---
+++ Korners.css
@@ -0,0 +1,55 @@
+/*
+    Copyright AppleSocialUK
+    Korners
+    
+    Support:
+    IE5.5 to Latest
+    Safari latest
+    Mozilla latest
+    Chrome latest
+    Opera latest
+*/
+
+/* Rounded Corners 5px */
+body h1,h2,h3,h4,h5,h6,p,span {
+  font-family:Arial, Verdana, Helvetica;
+  font-size:1em;
+}
+.r_c_5 {
+border-top-left-radius:5px 5px; 
+border-top-right-radius:5px 5px; 
+border-bottom-left-radius:5px 5px; 
+border-bottom-right-radius:5px 5px;
+}
+/* Rounded Corners 10px */
+.r_c_10 {
+border-top-left-radius:10px 10px; 
+border-top-right-radius:10px 10px; 
+border-bottom-left-radius:10px 10px; 
+border-bottom-right-radius:10px 10px;
+}
+/* Gray drop shadow */
+.g_d_s {
+-moz-box-shadow: 3px 3px 4px #999; /* Firefox */
+    -webkit-box-shadow: 3px 3px 4px #999; /* Safari/Chrome */
+    box-shadow: 3px 3px 4px #999; /* Opera and other CSS3 supporting    browsers */
+    -ms-filter: "progid:DXImageTransform.Microsoft.Shadow(Strength=4,    Direction=135, Color='#999999')";/* IE 8 */
+    : progid:DXImageTransform.Microsoft.Shadow(Strength=4, Direction=135,    Color='#999999');/* IE 5.5 - 7 */ 
+}
+/* Red drop shadow */
+.r_d_s {
+-moz-box-shadow: 3px 3px 4px #FF0033; /* Firefox */
+    -webkit-box-shadow: 3px 3px 4px #FF0033; /* Safari/Chrome */
+    box-shadow: 3px 3px 4px #FF0033; /* Opera and other CSS3 supporting    browsers */
+    -ms-filter: "progid:DXImageTransform.Microsoft.Shadow(Strength=4,    Direction=135, Color='#FF0033')";/* IE 8 */
+    : progid:DXImageTransform.Microsoft.Shadow(Strength=4, Direction=135,    Color='#FF0033');/* IE 5.5 - 7 */ 
+}    
+/* Border Bottom Gray 1px */
+.b_b_g_1 {
+        border-bottom:1px solid #999999;
+}
+/* Border Bottom Green 2px */
+.b_b_g_1 {
+        border-bottom:2px solid #00FF00;
+}
+
